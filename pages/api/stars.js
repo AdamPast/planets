1049880872229abd29602da8093faeffaa61bab2
AdamPast <a.pastalenz@gmail.com>
2022-06-18 14:31:39 +0200

    fixed some api calls

--- pages/api/stars.js
+++
@@ -1,8 +0,0 @@
-import { supabaseAdmin } from '../../utils/supabase';
-export default function handler(req, res) {
-  supabaseAdmin
-    .from('stars')
-    .select('*')
-    .then((result) => res.status(200).json({ data: result.data }))
-    .catch((err) => res.status(500).json({ error: err }));
-}
